c3a3ee2d22a08770bce3a236c2f9a75526e2ab65
George Diavatis <35267983+cloud-commander@users.noreply.github.com>
2020-06-10 22:05:52 +0100

    Update partition.py

--- utils/partition.py
+++ utils/partition.py
@@ -1,22 +1,10 @@
 import shutil
+import os
 
 
-def iterate_dir(source, dest, ratio):
-    source = source.replace('\\', '/')
-    dest = dest.replace('\\', '/')
-    train_dir = os.path.join(dest, 'train')
-    print(train_dir)
-    test_dir = os.path.join(dest, 'test')
-    print(test_dir)
-    
-    if not os.path.exists(train_dir):
-        os.makedirs(train_dir)
-    
-    if not os.path.exists(test_dir):
-        os.makedirs(test_dir)
-
-        
-    images = [f for f in os.listdir(source)
+def partition_dir(src_img, src_xml, train_img, train_xml, test_img, test_xml, ratio):
+       
+    images = [f for f in os.listdir(src_img)
               if re.search(r'([a-zA-Z0-9\s_\\.\-\(\):])+(.jpg|.jpeg|.png)$', f)]
 
      
@@ -29,27 +17,27 @@ def iterate_dir(source, dest, ratio):
         idx = random.randint(0, len(images)-1)
         
         filename = images[idx]
-        filename_path = os.path.join(source, filename)
+        filename_path = os.path.join(src_img, filename)
         
         xml_filename = os.path.splitext(filename)[0]+'.xml'
-        xml_path = os.path.join(source, xml_filename)
+        xml_path = os.path.join(src_xml, xml_filename)
         
         #copy image + XML files across only if accompanying XML file exists
         if os.path.isfile(xml_path):     
-                shutil.copyfile(filename_path, os.path.join(test_dir, filename))
-                shutil.copyfile(xml_path,os.path.join(test_dir,xml_filename))
+                shutil.copyfile(filename_path, os.path.join(test_img, filename))
+                shutil.copyfile(xml_path,os.path.join(test_xml,xml_filename))
         else:
             print("WARNING: the following XML file does not exist:" + xml_path)
         images.remove(images[idx])
 
     for filename in images:
         xml_filename = os.path.splitext(filename)[0]+'.xml'
-        xml_path = os.path.join(source, xml_filename)
-        filename_path = os.path.join(source, filename)
+        xml_path = os.path.join(src_xml, xml_filename)
+        filename_path = os.path.join(src_img, filename)
         
         #copy image + XML files across only if accompanying XML file exists
         if os.path.isfile(xml_path):
-                shutil.copyfile(filename_path, os.path.join(train_dir, filename))
-                shutil.copyfile(xml_path, os.path.join(train_dir, xml_filename))
+                shutil.copyfile(filename_path, os.path.join(train_img, filename))
+                shutil.copyfile(xml_path, os.path.join(train_xml, xml_filename))
         else:
             print("WARNING: the following XML file does not exist:" + xml_path)
