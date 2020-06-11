0b1fe6e52a7ffccd82d174ddb280cca8158e80c8
George Diavatis <35267983+cloud-commander@users.noreply.github.com>
2020-06-11 08:49:02 +0100

    Delete get_tf_models.sh

--- config/get_tf_models.sh
+++
@@ -1,4 +0,0 @@
-#Download Tensorflow models & tools
-mkdir temp && cd temp
-git clone https://github.com/tensorflow/models.git && (cd models && git checkout f788046ca876a8820e05b0b48c1fc2e16b0955bc)
-cp -r /content/temp/* /content/tensorflow/ && rm -R /content/*
