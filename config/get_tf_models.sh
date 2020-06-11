ae9fa0a02b23f7eccc0aa7f1be6d8d8acd0cba4c
George Diavatis <35267983+cloud-commander@users.noreply.github.com>
2020-06-11 08:24:00 +0100

    Update get_tf_models.sh

--- config/get_tf_models.sh
+++ config/get_tf_models.sh
@@ -1,8 +1,8 @@
 #Download Tensorflow models & tools
-cd {BASE_DIR}
+cd /content/tensorflow
 mkdir temp
 cd temp
 git clone https://github.com/tensorflow/models.git && (cd models && git checkout f788046ca876a8820e05b0b48c1fc2e16b0955bc)
-mv models {BASE_DIR}
-cd {BASE_DIR}
-rmdir {BASE_DIR}/temp
+mv models /content/tensorflow
+cd /content/tensorflow
+rmdir /content/tensorflow/temp
