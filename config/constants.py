0f2b06e419ad863b31670ecac53d52a8ea5056fe
George Diavatis <35267983+cloud-commander@users.noreply.github.com>
2020-06-16 19:16:34 +0100

    Update constants.py

--- config/constants.py
+++ config/constants.py
@@ -1,8 +1,55 @@
 # Configuration file to set the various directories needed for the project
 
+##################### DIRECTORY STRUCTURE ###############################
+#- content
+#	- models
+#		- official
+#		- research
+#		- samples
+#		- tutorials
+#		- pre-trained-model	
+#	- drive
+#		- Machine Learning
+#			- Datasets
+#			- Projects
+#				- project_dir
+#					- Dev
+#					- Complete			
+#	- project_dir
+#		- dataset
+#			- unprepapared
+#				- images
+#				- annotations
+#				- mask
+#			- prepared
+#				- train
+#					- images
+#					- annotations
+#				- test
+#					- images
+#					- annotations
+#			- records	
+#			- tmp1
+#			- tmp2							
+#		- scripts
+#			- preprocessing		
+#				
+#		- training
+#			- checkpoints
+#			- config
+#			- trained_graphs
+#				- inference_graph
+#				- tf_lite_graph
+#			- trained-models
+#				- tflite
+#				- tfjs
+#############################################################################
+
+
 ### Enter the name of the project ####
 PROJECT_DIR = "/Face-Mask-Detection" #
 ######################################
+
 
 
 ### Your Google Drive Folders - ***CREATE THESE MANUALLY***
@@ -11,20 +58,15 @@ DRIVE_ML = DRIVE_BASE + "/'Machine Learning'"
 DRIVE_DATASETS = DRIVE_ML + "/Datasets"
 DRIVE_PROJECTS = DRIVE_ML + "/Projects"
 DRIVE_TF = DRIVE_ML + "/TensorFlow"
-DRIVE_TF_MODELS = DRIVE_TF + "/Models"
-DRIVE_TF_RESEARCH = DRIVE_TF + "/research"
 DRIVE_CURRENT_PROJECT = DRIVE_PROJECTS + PROJECT_DIR 
 DRIVE_DEV = DRIVE_CURRENT_PROJECT + "/Dev/"
 DRIVE_COMPLETE = DRIVE_CURRENT_PROJECT + "/Complete/"
 
 #Sets the base directory for our project"
-BASE_DIR = "/content/tensorflow"
-
-#Sets the workspace directory 
-OBJ_DET_DIR = BASE_DIR + "/workspace"
+BASE_DIR = "/content"
 
 #Sets the project folder directory
-LEARN_DIR = OBJ_DET_DIR + PROJECT_DIR
+LEARN_DIR = BASE_DIR + PROJECT_DIR
 
 #Sets the unprepared dataset directoies
 DATASET_DIR = LEARN_DIR + "/dataset"
@@ -50,43 +92,24 @@ DATASET_DIR_TMP2 = DATASET_DIR + "/tmp2"
 #Sets the preprocessing scripts directory
 PRE_PROCESS_DIR = LEARN_DIR + "/scripts/preprocessing"
 
-PRE_TRAINED_MODEL_DIR = "/content/models/pre-trained-model"
-
 #Sets the training directory
 TRAIN_DIR = LEARN_DIR + "/training"
 
 #Sets the config file directory
 CONFIG_DIR = TRAIN_DIR + "/config/"
 
-#Sets the training model directory
-MDL_DIR = TRAIN_DIR + "/model"
-
 #Sets the checkpoint directory
-CKPT_DIR = MDL_DIR + "/checkpoints"
+CKPT_DIR = TRAIN_DIR + "/checkpoints"
 
 #Sets the trained graph directories
-GRAPH_DIR = MDL_DIR + "/trained_graphs"
+GRAPH_DIR = TRAIN_DIR + "/trained_graphs"
 INF_GRAPH = GRAPH_DIR + "/inference_graph"
 TF_LITE_GRAPH = GRAPH_DIR + "/tf_lite_graph"
 
 #Sets the trained model directories
-TRAINED_MODELS = MDL_DIR + "/trained-models"
+TRAINED_MODELS = TRAIN_DIR+ "/trained-models"
 TF_LITE_MODEL = TRAINED_MODELS + "/tflite"
 TF_JS_MODEL = TRAINED_MODELS + "/tfjs"
-
-# Loads a list with the edges of the required directory structure
-DIRECTORIES = [PRE_PROCESS_DIR, DATASET_DIR_UNPREP_IMG, \
-               DATASET_DIR_UNPREP_ANNO, DATASET_DIR_PREP_TRAIN_IMG, \
-               DATASET_DIR_PREP_TRAIN_ANNO, DATASET_DIR_PREP_TEST_IMG, \
-               DATASET_DIR_PREP_TEST_ANNO, DATASET_DIR_PREP_RECORD, \
-	       DATASET_DIR_TMP1, DATASET_DIR_TMP2, CONFIG_DIR,  \
-               INF_GRAPH, TF_LITE_GRAPH, CKPT_DIR, PRE_TRAINED_MODEL_DIR , \
-               TF_LITE_MODEL, TF_JS_MODEL  ]
-
-#create the required directories
-import os
-for DIR in DIRECTORIES:
-    os.makedirs(DIR, exist_ok=True)
 
 #Tensors for model conversion
 INPUT_TENSORS = "normalized_input_image_tensor"
@@ -117,3 +140,17 @@ PRETRAINED_MODEL_DIR= "/content/models/" + PRETRAINED_MODEL_NAME
 PRETRAINED_MODEL_CONFIG = "https://raw.githubusercontent.com/cloud-commander/face-mask-detection/master/config/pipeline.config"
 
 #PRETRAINED_MODEL_URL = "http://download.tensorflow.org/models/object_detection/ssd_mobilenet_v2_quantized_300x300_coco_2019_01_03.tar.gz"
+
+# Loads a list with the edges of the required directory structure
+DIRECTORIES = [PRE_PROCESS_DIR, DATASET_DIR_UNPREP_IMG, \
+               DATASET_DIR_UNPREP_ANNO, DATASET_DIR_PREP_TRAIN_IMG, \
+               DATASET_DIR_PREP_TRAIN_ANNO, DATASET_DIR_PREP_TEST_IMG, \
+               DATASET_DIR_PREP_TEST_ANNO, DATASET_DIR_PREP_RECORD, \
+	       DATASET_DIR_TMP1, DATASET_DIR_TMP2, CONFIG_DIR,  \
+               INF_GRAPH, TF_LITE_GRAPH, CKPT_DIR, PRETRAINED_MODEL_DIR , \
+               TF_LITE_MODEL, TF_JS_MODEL  ]
+
+#create the required directories
+import os
+for DIR in DIRECTORIES:
+    os.makedirs(DIR, exist_ok=True)
